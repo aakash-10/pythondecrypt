644f2bdb1a04e9f327d4e4012064e546bb3f0ebe
Python8100@ <aakash@MacBook-Air.Dlink>
2021-05-23 22:32:15 +0530

    gunicorn

--- app.py
+++ app.py
@@ -25,4 +25,6 @@ def hello():
 @app.route('/hello')
 def test():
     return {"message":"hello"}
-app.run(host='0.0.0.0', port=5000,debug=True)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
